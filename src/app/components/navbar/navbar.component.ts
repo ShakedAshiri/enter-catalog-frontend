b808991169363b23eb0848657cf0d72fbbd01481
Shaked <shakedashiri123@gmail.com>
2025-01-21 19:46:59 +0200

    Updated login button display

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   imports: [MatToolbarModule,
-            MatButtonModule],
+            MatButtonModule,
+            MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
