c266585ef470a115766e49716739ef377ec229b1
Shaked <shakedashiri123@gmail.com>
2025-01-26 13:43:16 +0200

    set user card color by category (tag)

--- src/app/components/homepage/users-grid/user-card/user-card.component.ts
+++ src/app/components/homepage/users-grid/user-card/user-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { User } from '../../../../shared/models/user.class';
+import { Category } from '../../../../shared/models/data-tables/category.class';
 
 @Component({
   selector: 'app-user-card',
@@ -10,4 +11,8 @@ import { User } from '../../../../shared/models/user.class';
 })
 export class UserCardComponent {
   @Input() user: User;
+
+  getCategoryClass(category: Category): string {
+    return `user-card--${category.data || 'default'}`;
+  }
 }
