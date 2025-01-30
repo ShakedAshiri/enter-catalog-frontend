be581063f741f379d0df6514546e3f2b30b513db
Shaked <shakedashiri123@gmail.com>
2025-01-30 14:57:11 +0200

    integration with backend

--- src/app/shared/services/data-table.service.ts
+++ src/app/shared/services/data-table.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Category } from '../models/data-tables/category.class';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataTableService {
+  private apiUrl = 'https://enter-catalog-backend-uw98.onrender.com/data-table/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  getCategories() {
-    return [
-      new Category("1", "animator", "אנימציה"),
-      new Category("2", "uiux", "UI\\UX"),
-      new Category("3", "developer", "פיתוח תוכנה")
-    ]
+  public getCategories():  Observable<Category[]>  {
+    return this.http.get<Category[]>(this.apiUrl + "categories");
   }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,64 +1,17 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.class';
-import { Category } from '../models/data-tables/category.class';
-import { UserRole } from '../models/data-tables/userRole.class';
-import { Status } from '../models/data-tables/status.class';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  // TODO: DELETE
-  animator = new Category("1", "animator", "אנימציה");
-  uiux = new Category("2", "uiux", "UI\\UX");
-  developer = new Category("3", "developer", "פיתוח תוכנה");
+  private apiUrl = 'https://enter-catalog-backend-uw98.onrender.com/';
 
-  // TODO: DELETE
-  categories: Category[] = [
-    this.animator, this.uiux, this.developer
-  ]
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-  public getUsers() {
-    return [
-        new User("1", "moshe-avrahami", "משה-אברהמי", "123",
-                 new UserRole("1", "1"), false, new Status("1", "1"),
-                 "עיצוב גרפי", "בוגר המחלקה לעיצוב חזותי ורוחני באקדמיה", [this.animator]),
-        new User("1", "dana-gotman", "דנה גוטמן", "123",
-                  new UserRole("1", "1"), false, new Status("1", "1"),
-                  "UI\\UX", "מפתחת ui\\ux עם 3 שנות נסיון בתחום.", [this.uiux]),
-        new User("1", "rotem-levi", "רותם לוי", "123",
-                  new UserRole("1", "1"), false, new Status("1", "1"),
-                  "אנימציה ופיתוח משחקים", "סיים תואר בהצטיינות יתרה", [this.developer]),
-        new User("1", "meital nachmani", "מיטל נחמני", "123",
-                 new UserRole("1", "1"), false, new Status("1", "1"),
-                 "עיצוב גרפי", "בוגר המחלקה לעיצוב חזותי ורוחני באקדמיה", [this.animator, this.uiux]),
-        new User("1", "roy-amadi", "רועי עמדי", "123",
-                 new UserRole("1", "1"), false, new Status("1", "1"),
-                 "UI\UX", "מפתחת ui\\ux עם 3 שנות נסיון בתחום.", [this.uiux]),
-        new User("1", "ela-advaa", "אלה אדווה", "123",
-                 new UserRole("1", "1"), false, new Status("1", "1"),
-                 "אנימציה ופיתוח משחקים", "סיים תואר בהצטיינות יתרה", [this.animator]),
-        new User("1", "moshe-avrahami", "משה-אברהמי", "123",
-                  new UserRole("1", "1"), false, new Status("1", "1"),
-                  "עיצוב גרפי", "בוגר המחלקה לעיצוב חזותי ורוחני באקדמיה", [this.uiux]),
-        new User("1", "dana-gotman", "דנה גוטמן", "123",
-                   new UserRole("1", "1"), false, new Status("1", "1"),
-                   "UI\\UX", "מפתחת ui\\ux עם 3 שנות נסיון בתחום.", [this.uiux]),
-        new User("1", "rotem-levi", "רותם לוי", "123",
-                   new UserRole("1", "1"), false, new Status("1", "1"),
-                   "אנימציה ופיתוח משחקים", "סיים תואר בהצטיינות יתרה", [this.animator]),
-        new User("1", "michal-shalom", "מיכל שלום", "123",
-                  new UserRole("1", "1"), false, new Status("1", "1"),
-                  "עיצוב גרפי", "בוגר המחלקה לעיצוב חזותי ורוחני באקדמיה", [this.uiux]),
-        new User("1", "roy-amadi", "רועי עמדי", "123",
-                  new UserRole("1", "1"), false, new Status("1", "1"),
-                  "UI\UX", "מפתחת ui\\ux עם 3 שנות נסיון בתחום.", [this.uiux]),
-        new User("1", "ela-advaa", "אלה אדווה", "123",
-                  new UserRole("1", "1"), false, new Status("1", "1"),
-                  "אנימציה ופיתוח משחקים", "סיים תואר בהצטיינות יתרה", [this.animator])
-
-      ];
+  public getUsers():  Observable<User[]>  {
+    return this.http.get<User[]>(this.apiUrl + "user");
   }
 }
