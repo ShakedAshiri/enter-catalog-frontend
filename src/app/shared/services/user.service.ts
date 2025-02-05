e12e6ce34d07c1b802018df725c55192eb9050a0
Shaked <shakedashiri123@gmail.com>
2025-02-05 13:40:06 +0200

    Created centralized api constants file

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@angular/core';
 import { User } from '../models/user.class';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ApiConstants } from '../constants/api.constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'https://enter-catalog-backend-uw98.onrender.com/';
 
   constructor(private http: HttpClient) {}
 
   public getUsers():  Observable<User[]>  {
-    return this.http.get<User[]>(this.apiUrl + "user");
+    return this.http.get<User[]>(ApiConstants.ENDPOINTS.USERS.ALL_USERS);
   }
 
   public getWorkers():  Observable<User[]>  {
-    return this.http.get<User[]>(this.apiUrl + "user/workers");
+    return this.http.get<User[]>(ApiConstants.ENDPOINTS.USERS.WORKERS);
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://enter-catalog-backend-uw98.onrender.com'
+};
