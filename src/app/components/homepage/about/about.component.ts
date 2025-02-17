592d875046dde098508d3f4ba46dcb5cfd86d959
Shaked <shakedashiri123@gmail.com>
2025-02-17 21:16:13 +0200

    added responsive design

--- src/app/components/homepage/about/about.component.ts
+++ src/app/components/homepage/about/about.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+  @ViewChild('aboutDesc') textContainer!: ElementRef;
+  isOverflowing = false;
+  readMore = false;
+
+  ngAfterViewInit() {
+    const el = this.textContainer.nativeElement;
+    console.log(el.scrollHeight)
+    console.log(el.clientHeight)
+    this.isOverflowing = el.scrollHeight > el.clientHeight;
+  }
+
 
 }
