5f031f315c59a7d8c74b8df9701ae5b0ada01e52
Shaked <shakedashiri123@gmail.com>
2025-03-04 13:07:21 +0200

    added option to send data to pop up window

--- src/app/shared/services/popup-modal.service.ts
+++ src/app/shared/services/popup-modal.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
-import { MatDialog, MatDialogRef, MatDialogConfig } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogRef,
+  MatDialogConfig,
+} from '@angular/material/dialog';
 import { ComponentType } from '@angular/cdk/portal';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PopupModalService {
-
   constructor(private dialog: MatDialog) {}
 
   // Generic method to open any component as a modal
-  open<T, D = any, R = any>(component: ComponentType<T>,
-                            config: MatDialogConfig<D> = {}): MatDialogRef<T, R> {
-
+  open<T, D = any, R = any>(
+    component: ComponentType<T>,
+    config: MatDialogConfig<D> = {},
+    data?: any
+  ): MatDialogRef<T, R> {
     // Set default configuration
     const defaultConfig: MatDialogConfig = {
       width: '500px',
       autoFocus: false,
-      ...config
+      data: data,
+      ...config,
     };
 
     return this.dialog.open<T, D, R>(component, defaultConfig);
