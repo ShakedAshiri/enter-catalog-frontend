5af4f954a772454c9435215402297158a5695298
Shaked <shakedashiri123@gmail.com>
2025-03-10 22:14:38 +0200

    continue "create work" modal creation

--- src/app/components/profile-page/user-work-modal/user-work-modal.component.ts
+++ src/app/components/profile-page/user-work-modal/user-work-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { ModalWrapperComponent } from '../../../shared/components/modal-wrapper/modal-wrapper.component';
 import { BaseModalComponent } from '../../../shared/components/base-modal/base-modal.component';
 import { ServerErrorComponent } from '../../../shared/components/server-error/server-error.component';
@@ -7,12 +7,18 @@ import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { HiddenSubmitComponent } from '../../../shared/components/hidden-submit/hidden-submit.component';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { UserWork } from '../../../shared/models/userWork.class';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
+import noOnlySpacesValidator from '../../../shared/validators/no-only-spaces.validator';
 
 @Component({
   selector: 'app-user-work-modal',
@@ -23,7 +29,11 @@ import { MatProgressSpinner } from '@angular/material/progress-spinner';
     MatFormFieldModule,
     MatDialogModule,
     HiddenSubmitComponent,
-    MatProgressSpinner,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    NgIf,
   ],
   templateUrl: './user-work-modal.component.html',
   styleUrl: './user-work-modal.component.scss',
@@ -31,11 +41,54 @@ import { MatProgressSpinner } from '@angular/material/progress-spinner';
 export class UserWorkModalComponent extends BaseModalComponent {
   showServerError: boolean = false;
   isFormSubmitting: boolean = false;
-  private subscriptions: Subscription[] = [];
 
   private fb = inject(FormBuilder);
-  form: FormGroup;
-  tempPassControl: FormControl; // TODO: different control
+  userWorkForm: FormGroup;
+  titleControl: FormControl;
+  descriptionControl: FormControl;
+  imageControl: FormControl;
 
-  submit() {}
+  userWork: UserWork;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data?: any) {
+    super();
+
+    this.userWork = data?.userWork ?? null;
+
+    // Create form controls
+    this.titleControl = this.fb.control(
+      this.userWork ? this.userWork.title : '',
+      [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(100),
+        Validators.pattern("^[a-zA-Z\u0590-\u05FF\u200f\u200e '-]+$"),
+        noOnlySpacesValidator(),
+      ]
+    );
+    this.descriptionControl = this.fb.control(
+      this.userWork ? this.userWork.description : '',
+      [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(500),
+        Validators.pattern("^[a-zA-Z\u0590-\u05FF\u200f\u200e\n '-,.!?;]+$"),
+        noOnlySpacesValidator(),
+      ]
+    );
+    this.imageControl = this.fb.control(
+      this.userWork && this.userWork.images ? this.userWork.images[0].url : ''
+    );
+
+    // Create form
+    this.userWorkForm = this.fb.group({
+      title: this.titleControl,
+      description: this.descriptionControl,
+      image: this.imageControl,
+    });
+  }
+
+  submit() {
+    this.dialogRef.close(this.userWork);
+  }
 }
--- src/app/components/profile-page/user-works/user-works.component.ts
+++ src/app/components/profile-page/user-works/user-works.component.ts
@@ -1,18 +1,34 @@
+import { PopupModalService } from './../../../shared/services/popup-modal.service';
+import { MatIconModule } from '@angular/material/icon';
 import { Component, Input } from '@angular/core';
 import { UserWork } from '../../../shared/models/userWork.class';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { JsonPipe, NgFor, NgIf } from '@angular/common';
 import { LanguageService } from '../../../shared/services/language.service';
+import { UserWorkModalComponent } from '../user-work-modal/user-work-modal.component';
 
 @Component({
   selector: 'app-user-works',
-  imports: [MatGridListModule, MatCardModule, NgFor, NgIf],
+  imports: [MatGridListModule, MatCardModule, NgFor, NgIf, MatIconModule],
   templateUrl: './user-works.component.html',
   styleUrl: './user-works.component.scss',
 })
 export class UserWorksComponent {
   @Input({ required: true }) userWorks!: UserWork[];
+  @Input() isEditable: boolean = false;
 
-  constructor(protected readonly languageService: LanguageService) {}
+  constructor(
+    protected readonly languageService: LanguageService,
+    private popupModalService: PopupModalService
+  ) {}
+
+  openWorkModal(userWork?: UserWork) {
+    let dialogRef = this.popupModalService.open(UserWorkModalComponent);
+
+    // TODO: save result
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`); // Pizza!
+    });
+  }
 }
