1673be2549912c0d7e528f4b5226d1fbc67690ce
Shaked <shakedashiri123@gmail.com>
2025-03-11 16:02:20 +0200

    Added "choose image" functionality and text fields validation

--- src/app/components/profile-page/user-work-modal/user-work-modal.component.ts
+++ src/app/components/profile-page/user-work-modal/user-work-modal.component.ts
@@ -19,6 +19,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { NgIf } from '@angular/common';
 import noOnlySpacesValidator from '../../../shared/validators/no-only-spaces.validator';
+import { ImageService } from '../../../shared/services/image.service';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-user-work-modal',
@@ -34,6 +36,7 @@ import noOnlySpacesValidator from '../../../shared/validators/no-only-spaces.val
     MatInputModule,
     MatIconModule,
     NgIf,
+    MatProgressSpinner,
   ],
   templateUrl: './user-work-modal.component.html',
   styleUrl: './user-work-modal.component.scss',
@@ -41,6 +44,7 @@ import noOnlySpacesValidator from '../../../shared/validators/no-only-spaces.val
 export class UserWorkModalComponent extends BaseModalComponent {
   showServerError: boolean = false;
   isFormSubmitting: boolean = false;
+  isImageLoading: boolean = false;
 
   private fb = inject(FormBuilder);
   userWorkForm: FormGroup;
@@ -50,7 +54,10 @@ export class UserWorkModalComponent extends BaseModalComponent {
 
   userWork: UserWork;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data?: any) {
+  constructor(
+    private imageService: ImageService,
+    @Inject(MAT_DIALOG_DATA) public data?: any,
+  ) {
     super();
 
     this.userWork = data?.userWork ?? null;
@@ -64,7 +71,7 @@ export class UserWorkModalComponent extends BaseModalComponent {
         Validators.maxLength(100),
         Validators.pattern("^[a-zA-Z\u0590-\u05FF\u200f\u200e '-]+$"),
         noOnlySpacesValidator(),
-      ]
+      ],
     );
     this.descriptionControl = this.fb.control(
       this.userWork ? this.userWork.description : '',
@@ -74,10 +81,10 @@ export class UserWorkModalComponent extends BaseModalComponent {
         Validators.maxLength(500),
         Validators.pattern("^[a-zA-Z\u0590-\u05FF\u200f\u200e\n '-,.!?;]+$"),
         noOnlySpacesValidator(),
-      ]
+      ],
     );
     this.imageControl = this.fb.control(
-      this.userWork && this.userWork.images ? this.userWork.images[0].url : ''
+      this.userWork && this.userWork.images ? this.userWork.images[0].url : '',
     );
 
     // Create form
@@ -90,5 +97,32 @@ export class UserWorkModalComponent extends BaseModalComponent {
 
   submit() {
     this.dialogRef.close(this.userWork);
+  }
+
+  onImageFileSelected(event: Event) {
+    this.isImageLoading = true;
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+
+    const file = input.files[0];
+
+    // Check if file is an image
+    if (!this.imageService.isFileTypeAllowed(file)) {
+      this.imageControl.setErrors({ invalidFileType: true });
+      input.value = '';
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      this.imageControl.setValue(e.target.result);
+      this.isImageLoading = false;
+    };
+
+    reader.readAsDataURL(file);
   }
 }
