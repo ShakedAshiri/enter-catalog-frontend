74fc1532a114c6996a96feb16adab80d568a2670
Shaked <shakedashiri123@gmail.com>
2025-03-17 20:13:18 +0200

    added thumbnail strip for displaying all work images

--- src/app/components/profile-page/user-work-modal/user-work-modal.component.ts
+++ src/app/components/profile-page/user-work-modal/user-work-modal.component.ts
@@ -1,7 +1,13 @@
-import { Component, Inject, inject } from '@angular/core';
-import { ModalWrapperComponent } from '../../../shared/components/modal-wrapper/modal-wrapper.component';
-import { ServerErrorComponent } from '../../../shared/components/server-error/server-error.component';
 import {
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  inject,
+  ViewChild,
+} from '@angular/core';
+import { ModalWrapperComponent } from '../../../shared/components/modal-wrapper/modal-wrapper.component';
+import {
+  FormArray,
   FormBuilder,
   FormControl,
   FormGroup,
@@ -18,12 +24,16 @@ import { NgIf } from '@angular/common';
 import noOnlySpacesValidator from '../../../shared/validators/no-only-spaces.validator';
 import { ImageService } from '../../../shared/services/image.service';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { WorkImage } from '../../../shared/models/workImage.class';
+import {
+  CdkVirtualScrollViewport,
+  ScrollingModule,
+} from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-user-work-modal',
   imports: [
     ModalWrapperComponent,
-    ServerErrorComponent,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatDialogModule,
@@ -33,30 +43,41 @@ import { MatProgressSpinner } from '@angular/material/progress-spinner';
     MatIconModule,
     NgIf,
     MatProgressSpinner,
+    ScrollingModule,
   ],
   templateUrl: './user-work-modal.component.html',
   styleUrl: './user-work-modal.component.scss',
 })
 export class UserWorkModalComponent extends ModalWrapperComponent {
-  showServerError: boolean = false;
   isFormSubmitting: boolean = false;
   isImageLoading: boolean = false;
+  override title: string = '';
 
   private fb = inject(FormBuilder);
   userWorkForm: FormGroup;
   titleControl: FormControl;
   descriptionControl: FormControl;
-  imageControl: FormControl;
+  imagesControl: FormArray<FormControl<WorkImage>>;
 
   userWork: UserWork;
+  selectedImage: WorkImage;
+  images: WorkImage[] = [];
+
+  @ViewChild(CdkVirtualScrollViewport) viewport!: CdkVirtualScrollViewport;
 
   constructor(
     private imageService: ImageService,
+    private cdr: ChangeDetectorRef,
     @Inject(MAT_DIALOG_DATA) public data?: any,
   ) {
     super();
 
+    this.title = data?.title;
     this.userWork = data?.userWork ?? null;
+
+    // For thumbnail scroll
+    this.selectedImage = this.userWork?.images[0] ?? null;
+    this.images = this.userWork?.images ?? [];
 
     // Create form controls
     this.titleControl = this.fb.control('', [
@@ -73,13 +94,17 @@ export class UserWorkModalComponent extends ModalWrapperComponent {
       Validators.pattern("^[a-zA-Z\u0590-\u05FF\u200f\u200e\n '-,.!?;]+$"),
       noOnlySpacesValidator(),
     ]);
-    this.imageControl = this.fb.control('');
+    this.imagesControl = this.fb.array(
+      this.userWork?.images.map((image) => this.fb.control(image)) || [
+        this.fb.control(null),
+      ],
+    );
 
     // Create form
     this.userWorkForm = this.fb.group({
       title: this.titleControl,
       description: this.descriptionControl,
-      image: this.imageControl,
+      images: this.imagesControl,
     });
 
     if (this.userWork) {
@@ -91,6 +116,11 @@ export class UserWorkModalComponent extends ModalWrapperComponent {
     const updatedUserWork: UserWork = {
       ...this.userWork,
       ...this.userWorkForm.value,
+      workFiles: [], // TODO - update so editable in form
+      // TODO: images - should send to backend as workImage array
+      images: this.userWorkForm.value.images.map(
+        (workImg: WorkImage) => workImg.url,
+      ),
     };
 
     this.close(updatedUserWork);
@@ -101,6 +131,7 @@ export class UserWorkModalComponent extends ModalWrapperComponent {
     const input = event.target as HTMLInputElement;
 
     if (!input.files || input.files.length === 0) {
+      this.isImageLoading = false;
       return;
     }
 
@@ -108,15 +139,29 @@ export class UserWorkModalComponent extends ModalWrapperComponent {
 
     // Check if file is an image
     if (!this.imageService.isFileTypeAllowed(file)) {
-      this.imageControl.setErrors({ invalidFileType: true });
+      this.imagesControl.setErrors({ invalidFileType: true });
       input.value = '';
+      this.isImageLoading = false;
       return;
     }
 
     const reader = new FileReader();
 
     reader.onload = (e: any) => {
-      this.imageControl.setValue(e.target.result);
+      // Remove initial placeholder if it's empty - relevant when editing work without initial image
+      if (this.imagesControl.length === 1 && !this.imagesControl.at(0).value) {
+        this.imagesControl.removeAt(0);
+      }
+
+      const newImg = new WorkImage(e.target.result);
+
+      this.imagesControl.push(this.fb.control(newImg));
+
+      this.selectedImage = newImg;
+
+      // Trigger reactivity - without this new thumbnail does not show
+      this.images = [...this.imagesControl.value];
+
       this.isImageLoading = false;
     };
 
