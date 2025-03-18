86c801315047a90b5afff232b805783f74c5acf8
Shaked <shakedashiri123@gmail.com>
2025-03-18 15:00:35 +0200

    Updated call to server to include only partial userWork

--- src/app/shared/services/user-works.service.ts
+++ src/app/shared/services/user-works.service.ts
@@ -10,16 +10,16 @@ export class UserWorksService {
 
   constructor(private http: HttpClient) {}
 
-  public createUserWork(userWork: UserWork) {
+  public createUserWork(userWork: Partial<UserWork>) {
     return this.http.post<UserWork>(
       ApiConstants.ENDPOINTS.WORKS.CREATE,
       userWork,
     );
   }
 
-  public updateUserWork(id: number, userWork: UserWork) {
+  public updateUserWork(userWorkId: number, userWork: Partial<UserWork>) {
     const url = ApiConstants.buildUrl(ApiConstants.ENDPOINTS.WORKS.UPDATE, {
-      id: id,
+      id: userWorkId,
     });
     return this.http.patch<UserWork>(url, userWork);
   }
