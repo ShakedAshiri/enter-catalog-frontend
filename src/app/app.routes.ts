4c6ba3fa115483794324dbe9ae0ecc92ee4a3516
Shaked <shakedashiri123@gmail.com>
2025-04-24 12:21:25 +0300

    Added control panel and user-management child components to dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ProfilePageComponent } from './components/profile-page/profile-page.com
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { hasRoleGuard } from './shared/guards/has-role.guard';
 import { Role } from './shared/constants/role';
+import { ControlPanelComponent } from './components/admin-dashboard/control-panel/control-panel.component';
+import { UsersManagementComponent } from './components/admin-dashboard/user-management/user-management.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -19,5 +21,10 @@ export const routes: Routes = [
     data: {
       roles: [Role.ADMIN, Role.TEAMLEAD],
     },
+    children: [
+      { path: '', redirectTo: 'control-panel', pathMatch: 'full' },
+      { path: 'control-panel', component: ControlPanelComponent },
+      { path: 'user-management', component: UsersManagementComponent }
+    ]
   },
 ];
