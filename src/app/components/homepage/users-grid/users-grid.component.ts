31d5df6cd60b884ac1ca19abc512e3fcc04bdb6b
Shaked <shakedashiri123@gmail.com>
2025-04-27 14:58:57 +0300

    added responisive design - on very large desktop - display 4 users in a row instead of 3

--- src/app/components/homepage/users-grid/users-grid.component.ts
+++ src/app/components/homepage/users-grid/users-grid.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { UserCardComponent } from "./user-card/user-card.component";
+import { UserCardComponent } from './user-card/user-card.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { CommonModule } from '@angular/common'; // Import CommonModule
 import { User } from '../../../shared/models/user.class';
@@ -8,15 +8,16 @@ import { User } from '../../../shared/models/user.class';
   selector: 'app-users-grid',
   imports: [CommonModule, UserCardComponent, MatGridListModule],
   templateUrl: './users-grid.component.html',
-  styleUrl: './users-grid.component.scss'
+  styleUrl: './users-grid.component.scss',
 })
 export class UsersGridComponent implements OnInit {
   @Input() users: User[] = [];
   breakpoint: number = 3;
-  rowHeight: string = "1:0.8";
+  rowHeight: string = '1:0.8';
 
   ngOnInit(): void {
-    this.breakpoint = (window.innerWidth <= 400) ? 2 : 3;
-    this.rowHeight = (window.innerWidth <= 400) ? "1:1" : "1:0.8";
+    this.breakpoint =
+      window.innerWidth <= 400 ? 2 : window.innerWidth >= 1800 ? 4 : 3;
+    this.rowHeight = window.innerWidth <= 400 ? '1:1' : '1:0.8';
   }
 }
