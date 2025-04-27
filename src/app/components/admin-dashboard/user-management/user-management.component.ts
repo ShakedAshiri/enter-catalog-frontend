a68c9875430d182feadf4ac65686ba686ee3ab9c
Shaked <shakedashiri123@gmail.com>
2025-04-27 16:44:59 +0300

    added workers table to admin dashboard

--- src/app/components/admin-dashboard/user-management/user-management.component.ts
+++ src/app/components/admin-dashboard/user-management/user-management.component.ts
@@ -1,11 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { User } from '../../../shared/models/user.class';
+import { UserService } from '../../../shared/services/user.service';
+import { Subscription } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { NgClass, NgIf } from '@angular/common';
+import { Category } from '../../../shared/models/data-tables/category.class';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ServerErrorComponent } from '../../../shared/components/server-error/server-error.component';
 
 @Component({
   selector: 'app-user-management',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatTableModule,
+    MatIconModule,
+    NgClass,
+    NgIf,
+    MatProgressSpinnerModule,
+    ServerErrorComponent,
+  ],
   templateUrl: './user-management.component.html',
-  styleUrl: './user-management.component.scss'
+  styleUrl: './user-management.component.scss',
 })
-export class UsersManagementComponent {
+export class UsersManagementComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+  private isProduction = environment.production;
 
+  displayedColumns: string[] = [
+    'image',
+    'displayName',
+    'isAvailable',
+    'branch',
+    'categories',
+    'actions',
+  ];
+  dataSource: MatTableDataSource<User>;
+  users: User[];
+
+  isUsersLoaded = false;
+  showUsersServerError = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.subscriptions.push(
+      this.userService.getWorkers().subscribe({
+        next: (response: User[]) => {
+          this.users = response;
+          this.dataSource = new MatTableDataSource(this.users);
+          this.isUsersLoaded = true;
+        },
+        error: (error) => {
+          if (!this.isProduction) console.error('Error fetching data:', error);
+          this.showUsersServerError = true;
+        },
+      }),
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+  getCatagories(catagories?: Category[]): string {
+    return catagories?.map((c) => c.displayName).join(', ') || '';
+  }
 }
