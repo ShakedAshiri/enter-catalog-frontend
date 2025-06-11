64d98fc1eaa47a2894feff3d9052bb87b5984a27
Shaked <shakedashiri123@gmail.com>
2025-06-11 22:49:58 +0300

    success modal update to display text sent as data variable

--- src/app/shared/components/contact-us/success-modal/success-modal.component.ts
+++ src/app/shared/components/contact-us/success-modal/success-modal.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Optional } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { ModalWrapperComponent } from '../../modal-wrapper/modal-wrapper.component';
 
 @Component({
   selector: 'app-success-modal',
-  imports: [],
+  imports: [MatIconModule, ModalWrapperComponent],
   templateUrl: './success-modal.component.html',
   styleUrl: './success-modal.component.scss',
 })
-export class SuccessModalComponent {}
+export class SuccessModalComponent {
+  text: string;
+
+  constructor(@Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
+    // Use the dialog data if available
+    if (data && data.text) {
+      this.text = data.text;
+    }
+  }
+}
