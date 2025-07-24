1ca4b0fd70073ab58f02deedf834e416e8f5b92c
Ron Barak <ronbarak2000@gmail.com>
2025-07-24 13:33:53 +0300

    contact-worker-modal -> conditionally render inputs div

--- src/app/components/profile-page/contact-worker-modal/contact-worker-modal.component.ts
+++ src/app/components/profile-page/contact-worker-modal/contact-worker-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-contact-worker-modal',
@@ -7,4 +8,10 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './contact-worker-modal.component.html',
   styleUrl: './contact-worker-modal.component.scss',
 })
-export class ContactWorkerModalComponent {}
+export class ContactWorkerModalComponent {
+  constructor(private readonly authService: AuthService) {}
+
+  get isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
+}
