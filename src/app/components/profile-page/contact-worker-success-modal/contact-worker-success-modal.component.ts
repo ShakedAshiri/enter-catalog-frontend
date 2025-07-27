d8b2d1bde65d47068b600b6e248c84a2096e82bd
Ron <ronbarak2000@gmail.com>
2025-07-27 14:17:39 +0300

    added x button to success modal

--- src/app/components/profile-page/contact-worker-success-modal/contact-worker-success-modal.component.ts
+++ src/app/components/profile-page/contact-worker-success-modal/contact-worker-success-modal.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { BaseModalComponent } from '../../../shared/components/base-modal/base-modal.component';
 
 @Component({
   selector: 'app-contact-worker-success-modal',
   imports: [],
   templateUrl: './contact-worker-success-modal.component.html',
-  styleUrl: './contact-worker-success-modal.component.scss'
+  styleUrl: './contact-worker-success-modal.component.scss',
 })
-export class ContactWorkerSuccessModalComponent {
-
+export class ContactWorkerSuccessModalComponent extends BaseModalComponent {
+  override submit() {
+    return null;
+  }
 }
